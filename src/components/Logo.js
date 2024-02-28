588c13b0dfeb78ccc2297a8123740c0e21579a98
nguyetmle <leminhnguyet2809@gmail.com>
2024-02-28 02:21:20 -0500

    delete logo

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import logoSvg from "../assets/logo.svg";
+// import logoSvg from "../assets/logo.svg";
 
 const Logo = () => {
   return (
